075aa0bd5aa4a39c17db1d8584e5286171f8ce29
김상현 <llmm030506@naver.com>
2021-07-06 14:48:32 +0900

    order 성능 조회

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -71,6 +71,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDtoOptimization();
+    }
+
     @Getter
     static class OrderDto {
 
